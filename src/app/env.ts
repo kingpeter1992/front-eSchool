237879f522412f6c81b1cde2ad297bc74773c0b1
kingpeter1992 <kingkapeta@gmail.com>
2026-08-19 13:27:13 +0200

    modification url v1

--- src/app/env.ts
+++ src/app/env.ts
@@ -1,6 +1,5 @@
 export const environment = {
   /*
-
   production: false,
   BASIC_URL: "http://127.0.0.1:8082/api/v1",
   FILE_URL: 'http://127.0.0.1:8082',
@@ -10,11 +9,11 @@ export const environment = {
   BASIC_URL_FORGOT: "http://127.0.0.1:8082/api/auth/forgot-password",
   BASIC_URL_RENITIALISATION: "http://127.0.0.1:8082/api/auth/reset-password",
   BASIC_URL_ADMIN: "http://127.0.0.1:8082/api/admin"
-
 */
 
+
    production: true,
-   BASIC_URL: "https://hschool-ccb7.onrender.com/api/",
+   BASIC_URL: "https://hschool-ccb7.onrender.com/api/v1",
    BASIC_URL_AUTH_LOGIN: "https://hschool-ccb7.onrender.com/api/auth/login",
   BASIC_URL_AUTH_ME: "https://hschool-ccb7.onrender.com/api/auth/me",
    BASIC_URL_REGISTER: "https://hschool-ccb7.onrender.com/api/auth/register",
@@ -22,9 +21,5 @@ export const environment = {
    BASIC_URL_RENITIALISATION: "https://hschool-ccb7.onrender.com/api/auth/reset-password",
    BASIC_URL_ADMIN: "https://hschool-ccb7.onrender.com/api/admin",
    FILE_URL: 'https://hschool-ccb7.onrender.com'
-
-
-
-
 
 };
