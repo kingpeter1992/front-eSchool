76cdf913764863e038f70d48f7c90a7b45fb12cb
kingpeter1992 <kingkapeta@gmail.com>
2026-08-19 12:27:54 +0200

    modification de url local en url render

--- src/app/env.ts
+++ src/app/env.ts
@@ -1,5 +1,5 @@
 export const environment = {
-
+  /*
 
   production: false,
   BASIC_URL: "http://127.0.0.1:8082/api/v1",
@@ -11,20 +11,20 @@ export const environment = {
   BASIC_URL_RENITIALISATION: "http://127.0.0.1:8082/api/auth/reset-password",
   BASIC_URL_ADMIN: "http://127.0.0.1:8082/api/admin"
 
-
-  /*
-   production: true,
-   BASIC_URL: "https://faceia-back.onrender.com/api/",
-   BASIC_URL_AUTH_LOGIN: "https://faceia-back.onrender.com/api/auth/login",
-  BASIC_URL_AUTH_ME: "https://faceia-back.onrender.com/api/auth/me",
-   BASIC_URL_REGISTER: "https://faceia-back.onrender.com/api/auth/register",
-   BASIC_URL_FORGOT: "https://faceia-back.onrender.com/api/auth/forgot-password",
-   BASIC_URL_RENITIALISATION: "https://faceia-back.onrender.com/api/auth/reset-password",
-   BASIC_URL_ADMIN: "https://faceia-back.onrender.com/api/admin",
-   FILE_URL: 'https://faceia-back.onrender.com'
-
-
-
 */
+
+   production: true,
+   BASIC_URL: "https://hschool-ccb7.onrender.com/api/",
+   BASIC_URL_AUTH_LOGIN: "https://hschool-ccb7.onrender.com/api/auth/login",
+  BASIC_URL_AUTH_ME: "https://hschool-ccb7.onrender.com/api/auth/me",
+   BASIC_URL_REGISTER: "https://hschool-ccb7.onrender.com/api/auth/register",
+   BASIC_URL_FORGOT: "https://hschool-ccb7.onrender.com/api/auth/forgot-password",
+   BASIC_URL_RENITIALISATION: "https://hschool-ccb7.onrender.com/api/auth/reset-password",
+   BASIC_URL_ADMIN: "https://hschool-ccb7.onrender.com/api/admin",
+   FILE_URL: 'https://hschool-ccb7.onrender.com'
+
+
+
+
 
 };
